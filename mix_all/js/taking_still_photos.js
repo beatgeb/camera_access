58a0d94b4abfa2e07e38c1fb8183dd83dbecedd2
Beat Gebistorf <beat.gebistorf@namics.com>
2014-11-28 14:50:16 +0100

    autoplay also for photo taking version

--- mix_all/js/taking_still_photos.js
+++ mix_all/js/taking_still_photos.js
@@ -19,6 +19,7 @@
             audio: false
         },
         function(stream) {
+            video.autoplay = true;
             if (navigator.mozGetUserMedia) {
                 video.mozSrcObject = stream;
             } else {
