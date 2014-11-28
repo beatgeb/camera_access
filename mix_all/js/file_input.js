da6e01fd89e72b6a06776abf1527bf0fe98d0d3a
Beat Gebistorf <beat.gebistorf@namics.com>
2014-11-28 15:59:45 +0100

    Add File upload test

---
+++ mix_all/js/file_input.js
@@ -0,0 +1,45 @@
+(function () {
+
+    var fileSelect = document.getElementById("fileSelect"),
+        fileElem = document.getElementById("fileElem");
+
+    fileSelect.addEventListener("click", function (e) {
+        if (fileElem) {
+            fileElem.click();
+        }
+        e.preventDefault(); // prevent navigation to "#"
+    }, false);
+
+})();
+
+function handleFiles(files) {
+    var preview = document.getElementById("preview");
+    preview.innerHTML = '';
+
+    for (var i = 0; i < files.length; i++) {
+        var file = files[i];
+        var imageType = /image.*/;
+
+        if (!file.type.match(imageType)) {
+            continue;
+        }
+
+        var metadata = document.createTextNode("Name: " + file.name + " Size: " + file.size + "bytes " + " Type: " + file.type);
+        var img = document.createElement("img");
+        img.classList.add("obj");
+        img.file = file;
+        preview.appendChild(document.createElement("br"));
+        preview.appendChild(metadata);
+        preview.appendChild(document.createElement("br"));
+        preview.appendChild(img);
+        preview.appendChild(document.createElement("br"));
+
+        var reader = new FileReader();
+        reader.onload = (function (aImg) {
+            return function (e) {
+                aImg.src = e.target.result;
+            };
+        })(img);
+        reader.readAsDataURL(file);
+    }
+}
